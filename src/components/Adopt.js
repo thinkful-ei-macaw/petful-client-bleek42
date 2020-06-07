bfecbd3339ada5d4b4c0d624f0bcd358ff720ee6
bleek42 <brandonleek42@gmail.com>
2020-06-07 13:06:54 -0400

    TypeError: Cannot convert undefined or null to object line 40 adopt js + several other errors need fixing

--- src/components/Adopt.js
+++ src/components/Adopt.js
@@ -38,32 +38,26 @@ class Adopt extends Component {
     const { people, pets } = this.state;
     return (
       <div className="adopt-page">
-        <section className="pets-section">
-          <h1>Pets</h1>
-          {Object.entries(pets).map(([pet, type]) => {
-            return pet ? (
-              <div>
-                <h4>{pet.name}</h4>
-                <img src={pet.imageURL} alt={pet.description} />
-                <section>
-                  <p>{pet.breed}</p>
-                  <p>{pet.gender}</p>
-                  <p>{pet.age}</p>
-                </section>
-                <form name="adoption" onClick={this.handleAdopt}>
-                  <label htmlFor="adopt-pet">Adopt {pet.name}</label>
-                  <input type="button" id="adopt-pet">
-                    Click to adopt
-                  </input>
-                </form>
-              </div>
-            ) : (
-              <div>
-                <h4>No pets not found!</h4>
-              </div>
-            );
-          })}
-        </section>
+        {Object.entries(pets).map(([pet]) => {
+          return pet ? (
+            <div>
+              <h4>{pet.name}</h4>
+              <img src={pet.imageURL} alt={pet.description} />
+              <section>
+                <p>{pet.breed}</p>
+                <p>{pet.gender}</p>
+                <p>{pet.age}</p>
+              </section>
+              <button name="adoption" onClick={() => this.handleAdopt}>
+                Adopt {pet.name}
+              </button>
+            </div>
+          ) : (
+            <div>
+              <h4>No pets found!</h4>
+            </div>
+          );
+        })}
         <section className="people-section">
           <h1>People</h1>
         </section>
