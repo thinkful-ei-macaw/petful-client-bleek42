57e2cec804880a762c2d39bf1b25e6f632805683
bleek42 <brandonleek42@gmail.com>
2020-06-13 22:01:46 -0400

    finally got people enqueue to render need to work on pets

--- src/components/Adopt.js
+++ src/components/Adopt.js
@@ -19,42 +19,53 @@ class Adopt extends Component {
   shelterStaff = ['Brandon Leek', 'Donna Leek', 'Liz Nye', 'Noah Jennsen'];
 
   componentDidMount() {
-    getPets();
-    getPeople();
-    this.interval = setInterval(() => {
-      const { people, user } = this.state;
-      if (!people.length) {
+    this.fetchPeople();
+    this.fetchPets();
+    // this.interval = setInterval(() => {
+    //   const { people, user } = this.state;
+    //   if (!people.length) {
+    //     this.setState({
+    //       error: true,
+    //     });
+    //   }
+    //   if (people[0] === user && people.length < 5) {
+    //     this.demoUsers();
+    //   } else if (people[0] !== user && people.length > 2) {
+    //     this.demoAdopt();
+    //   }
+    // }, 5000);
+  }
+
+  // componentWillUnmount() {
+  //   clearInterval(this.interval);
+  // }
+
+  fetchPeople = () => {
+    getPeople()
+      .then((person) =>
+        this.setState({
+          people: person.people,
+        })
+      )
+      .catch((error) => {
         this.setState({
           error: true,
         });
-      }
-      if (people[0] === user && people.length < 5) {
-        this.demoUsers();
-      } else if (people[0] !== user && people.length > 2) {
-        this.demoAdopt();
-      }
-    }, 5000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
-  fetchData = async () => {
-    try {
-      const pets = await getPets();
-      const people = await getPeople();
-      const peopleData = await people.json();
-      const petsData = await pets.json();
-      this.setState({
-        pets: petsData,
-        people: peopleData,
       });
-    } catch (error) {
-      this.setState({
-        error: true,
+  };
+
+  fetchPets = () => {
+    getPets()
+      .then((pet) =>
+        this.setState({
+          pets: pet.pets,
+        })
+      )
+      .catch((error) => {
+        this.setState({
+          error: true,
+        });
       });
-    }
   };
 
   demoUsers = () => {
@@ -99,28 +110,42 @@ class Adopt extends Component {
   };
 
   render() {
-    const { people, pets, staff, error } = this.state;
-    if (error === true) {
-      return (
-        <div className="error-render">
-          <h1>Uh-Oh!</h1>
-          <section>
-            <p>It appears something went wrong!</p>
-          </section>
-        </div>
-      );
-    } else {
+    const { people, pets, user, staff, error } = this.state;
+    console.log(this.state.people);
+    if (error === false) {
       return (
         <div className="adopt-page">
           <header className="adopt-header">
             <h1>Welcome to the adoption page!</h1>
           </header>
-          <section>
+          <section className="people-queue">
             <ul>
               {people.map((person) => (
-                <li key={person}>{person.name}</li>
+                <li name={person === user ? 'user' : ''} key={person[0]}>
+                  {person}
+                </li>
               ))}
             </ul>
+          </section>
+          <section className="pet-queue">
+            <ul>
+              {Object.entries(pets).map(([type, pet]) => {
+                return (
+                  <div key={pet.name}>
+                    <h4>{pet.name}</h4>
+                  </div>
+                );
+              })}
+            </ul>
+          </section>
+        </div>
+      );
+    } else {
+      return (
+        <div className="error-render">
+          <h1>Uh-Oh!</h1>
+          <section>
+            <p>It appears something went wrong!</p>
           </section>
         </div>
       );
